6e49fa5b7949fc65383bc0771050a4747f167389
puleugo <puleugo@gmail.com>
2025-04-17 22:34:37 +0900

    feat(domain): add Round class to represent a single race round

---
+++ src/main/kotlin/racingcar/domain/Round.kt
@@ -0,0 +1,19 @@
+package racingcar.domain
+
+data class Round(val index: Int, val cars: Cars) {
+
+    init {
+        require(index > 0) { "Round index must be a positive number." }
+    }
+
+    val winners: Cars
+        get() = cars.getMostMovedCar()
+
+    override fun toString(): String {
+        return StringBuilder().apply {
+            cars.forEach { car ->
+                append("${car.name} : ${"-".repeat(car.position)}\n")
+            }
+        }.toString()
+    }
+}
