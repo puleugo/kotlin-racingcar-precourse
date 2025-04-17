a7cac66b6fa41789e86faa76f2fec0c0bf844d96
puleugo <puleugo@gmail.com>
2025-04-17 22:31:01 +0900

    feat(application)!: implement main entrypoint for racing game

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,32 @@
 package racingcar
 
+import racingcar.domain.*
+
 fun main() {
-    // TODO: Implement the program
+        println("Enter the names of the cars (comma-separated):")
+        val rawCarNames = readLine()
+            ?.split(",")
+            ?.map { it.trim() }
+            ?: throw IllegalArgumentException("Car names cannot be empty.")
+        val cars = Cars( rawCarNames.map { Car(it) })
+
+        println("Enter the number of rounds:")
+        val roundCount = readLine()
+            ?.toIntOrNull()
+            ?: throw IllegalArgumentException("Number of rounds must be a positive number.")
+
+        val results = race(cars, roundCount)
+        val winnersCarNames = results
+            .getWinners().getCarNames()
+        println("Race Results\n"+results.toString()+"\nWinners : ${winnersCarNames.joinToString(", ")}")
+}
+
+fun race(cars: Cars, roundCount: Int): Rounds {
+    val roundsTemp = mutableListOf<Round>()
+    for (roundIndex in 1..roundCount) {
+        cars.moveRandomly()
+        val round = cars.createRound(roundIndex)
+        roundsTemp.add(round)
+    }
+    return Rounds(roundsTemp)
 }
